be172e0da7486cffad80bd097fd98b40da60ce5a
SohrobEVO <106800591+SohrobEVO@users.noreply.github.com>
2022-07-12 15:28:31 +0700

    est

--- pages/business/profile.js
+++ pages/business/profile.js
@@ -5,143 +5,144 @@ import { IconButton } from "@/components/icons/IconButton";
 import { LockIcon } from "@/components/icons/LockIcon";
 import { AiOutlineUser, AiFillMessage, AiOutlineHistory, AiTwotoneTags, AiTwotoneBell, ioIoIosMegaphone, AiOutlineCaretDown, } from "react-icons/ai";
 import { IoIosMegaphone, } from "react-icons/io";
+import { MdEmail, } from "react-icons/Md";
 import { BsPlus } from "react-icons/bs";
 import Image from 'next/image';
 import Layout from "@/components/layout";
 import ModelAddPartner from "@/components/partner/ModelAddPartner";
 import ModelEditPartner from "@/components/partner/ModelEditPartner";
-import { Col, Container, Grid, Input, Row, Spacer, Table, Tooltip } from "@nextui-org/react"; 
+import { Col, Container, Grid, Input, Row, Spacer, Table, Tooltip } from "@nextui-org/react";
 import Head from "next/head";
-import { useState } from 'react'                               
+import { useState } from 'react'
 
 export default function Business() {
     const [visible, setVisible] = useState(false);
 
     return (
         <Layout>
-            
+
             <Head>
-            
-                <title> Business | JONGKEW</title>  
+
+                <title> Business | JONGKEW</title>
                 <meta
                     name="description"
                     content="Business JONGKEW"
                 />
                 <link rel="icon" href="/favicon.ico" />
-                
+
             </Head>
-            <Container> 
-            <div class="filter">
+            <Container className="profile">
+                <div class="filter">
 
-</div>
+                </div>
 
-<table >
-
-
-
-<tr>
-
-<th>#</th>
-
-<th>Nom</th>
-
-<th>Prenom</th>
-
-<th>identifiant </th>
-
-<th>date de naissance </th>
-
-<th>salaire</th>
-
-</tr>
-
-
- <tr>
-
-<td></td>
-
-<td></td>
-
-<td>อีเมล</td>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-</tr>
-
-<tr>
-
-<td></td>
-
-<td></td>
-
-<td>ชื่อ-นามสกุล</td>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-</tr>
-
-<tr>
-
-<td></td>
-
-<td></td>
-
-<td>เบอร์โทรศัพท์</td>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-</tr>
-
-<tr>
-
-<td></td>
-
-<td></td>
-
-<td>ศูนย์ช่วยเหลือ</td>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-</tr>
-
-<tr>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-<td></td>
-
-</tr>
+                <table >
 
 
 
+                    <tr>
+
+                        <th>#</th>
+
+                        <th>Nom</th>
+
+                        <th>Prenom</th>
+
+                        <th>identifiant </th>
+
+                        <th>date de naissance </th>
+
+                        <th>salaire</th>
+
+                    </tr>
 
 
-</table>
-   
+                    <tr>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td><MdEmail size={20} />อีเมล</td>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td>ชื่อ-นามสกุล</td>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td>เบอร์โทรศัพท์</td>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td>ศูนย์ช่วยเหลือ</td>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                        <td></td>
+
+                    </tr>
+
+
+
+
+
+                </table>
+
             </Container>
         </Layout>
     );
