d92899c458d7b0102b9873e9efe10e2c82ac1f6e
SohrobEVO <106800591+SohrobEVO@users.noreply.github.com>
2022-07-13 13:02:09 +0700

    Update promotion.js

--- pages/business/promotion.js
+++ pages/business/promotion.js
@@ -1,23 +1,14 @@
 import React from "react";
-
-// components
-
-// import CardLineChart from "components/Cards/CardLineChart.js";
-// import CardBarChart from "components/Cards/CardBarChart.js";
-// import CardPageVisits from "components/Cards/CardPageVisits.js";
-// import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
-
-// layout for page
-
+import { AiOutlinePlus } from "react-icons/ai";
 import Business from "../../layouts/Business";
 
 export default function Dashboard() {
   return (
     <>
-    ฟหกฟหดฟ
+    <h2>เพิ่มโปรโมชั่นร้าน<AiOutlinePlus /></h2>
       <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          {/* <CardLineChart /> */}หหห  หห
+          {/* <CardLineChart /> */}
         </div>
         <div className="w-full xl:w-4/12 px-4">
           {/* <CardBarChart /> */}
