aeff14ffaf59ec479063f8b01869b13cac392454
SohrobEVO <106800591+SohrobEVO@users.noreply.github.com>
2022-07-13 14:05:09 +0700

    sohrob

--- pages/business/promotion.js
+++ pages/business/promotion.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AiOutlinePlus, AiFillTags, AiOutlineHistory, AiOutlineBell, } from "react-icons/ai";
-import { BsFillMegaphoneFill } from "react-icons/Bs";
+import { BsFillMegaphoneFill, BsFillChatDotsFill, } from "react-icons/Bs";
 import Business from "../../layouts/Business";
 import { Button, Container, Row, Col, } from "react-bootstrap";
 
@@ -11,30 +11,29 @@ export default function Dashboard() {
     <>
       <h2>เพิ่มโปรโมชั่นร้าน<AiOutlinePlus /></h2>
 
-    <Container>
-      <Row>
-        <Col></Col>
-        <Col><BsFillMegaphoneFill size={30} /><h1>โปรโมชั่น</h1></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-      </Row>
-    </Container>
-  
-
-
 
       <Container className="promotion">
+        <Row>
+          <Col></Col>
+          <Col><h1><BsFillMegaphoneFill size={50} />&nbsp;&nbsp;&nbsp;โปรโมชั่น</h1></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
         <div>
-          <Button variant="white"><AiOutlineHistory size={30} /></Button>{' '}
+          <Button variant="white"><BsFillChatDotsFill size={30} /></Button>{' '}
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <Button variant="white"><AiFillTags size={30} /></Button>{' '}
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <Button variant="white"><AiOutlineHistory size={30} /></Button>{' '}
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <Button variant="white"><AiOutlineBell size={30} /></Button>{' '}
+          <Row>
+            
+        <Col>1 of 1</Col>
+      </Row>
 
         </div>
 
