fc4e05f59491e0e1303ea7cb608bb80e9b5eeba5
shpiz33 <enderclockch@gmail.com>
2022-07-19 15:02:13 +0700

    index arm

--- components/business/IndexCardBusinee.jsx
+++ components/business/IndexCardBusinee.jsx
@@ -15,9 +15,7 @@ function IndexCardBusinee() {
             This is a wider card 
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
+        
       </Card>
 </Container>
   );
